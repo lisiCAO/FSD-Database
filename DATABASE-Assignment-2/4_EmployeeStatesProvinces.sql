840e23c425857581c54b6339a6b72ea5169a270a
lisiCAO <caolisi@hotmailcom>
2023-12-09 19:21:26 -0500

    rename

--- DATABASE-Assignment-2/4_EmployeeStatesProvinces.sql
+++
@@ -1,8 +0,0 @@
-/* Purpose: In what states or provinces do the employees reside */
-/* Script date: August 13, 2023 */
-/* Author: Lisi Cao */
-
--- 4_EmployeeStatesProvinces.sql
-SELECT DISTINCT 
-    state AS 'State'
-FROM employee;
